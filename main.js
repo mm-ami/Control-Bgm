2eaf9a9d2dfd3d6c71a48e1c59b8af47ccc3a5ce
mm-ami <mhju.9599@gmail.com>
2021-10-31 17:34:45 +0900

    main.js

--- main.js
+++ main.js
@@ -1,37 +1,37 @@
-const playback = document.querySelectorAll('.playback');
-let song = document.querySelectorAll('.song');
-let icon = document.querySelectorAll('.fa-play');
-const range = document.querySelectorAll('.volume');
-const audio = document.querySelectorAll('audio');
-
-console.log(range)
-
-for(let v = 0; v < playback.length; v++) {
-  function playsong() {
-    icon[v].classList.remove('fa-play');
-    icon[v].classList.add('fa-pause');
-    song[v].play();
-  };
-
-  function pausesong() {
-    icon[v].classList.add('fa-play');
-    icon[v].classList.remove('fa-pause');
-    song[v].pause();
-  };
-
-  playback[v].addEventListener('click', () => {
-    playback[v].classList.toggle('play');
-    const playing = playback[v].classList.contains('play');
-    if(playing) {
-      playsong();
-    } else {
-      pausesong();
-    }
-  });
-
-  audio[v].volume = range[v].value;
-
-  range[v].addEventListener('input', event => {
+document.addEventListener('DOMContentLoaded', function() {
+  const playback = document.querySelectorAll('.playback');
+  let song = document.querySelectorAll('.song');
+  let icon = document.querySelectorAll('.fa-play');
+  const range = document.querySelectorAll('.volume');
+  const audio = document.querySelectorAll('audio');
+  
+  for(let v = 0; v < playback.length; v++) {
+    function playsong() {
+      icon[v].classList.remove('fa-play');
+      icon[v].classList.add('fa-pause');
+      song[v].play();
+    };
+  
+    function pausesong() {
+      icon[v].classList.add('fa-play');
+      icon[v].classList.remove('fa-pause');
+      song[v].pause();
+    };
+  
+    playback[v].addEventListener('click', () => {
+      playback[v].classList.toggle('play');
+      const playing = playback[v].classList.contains('play');
+      if(playing) {
+        playsong();
+      } else {
+        pausesong();
+      }
+    });
+  
     audio[v].volume = range[v].value;
-  })
-}
+  
+    range[v].addEventListener('input', event => {
+      audio[v].volume = range[v].value;
+    });
+  };
+});
